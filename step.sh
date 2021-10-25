249a5c712fc103d7549facf0a698a954b7af0dd6
pokryfka <dev@pokryfka.net>
2021-10-25 19:58:48 +0800

    fix: lint project after fetching dependencies

--- step.sh
+++ step.sh
@@ -8,12 +8,6 @@ function log() {
 log "Installing Tuist ..."
 curl -Ls https://install.tuist.io | bash
 
-if ${lint_project}
-then
-    log "Linting project ..."
-    tuist lint project
-fi
-
 if ${lint_code}
 then
     log "Linting code ..."
@@ -24,6 +18,12 @@ if ${dependencies_fetch}
 then
     log "Fetching dependencies ..."
     tuist dependencies fetch
+fi
+
+if ${lint_project}
+then
+    log "Linting project ..."
+    tuist lint project
 fi
 
 if ${cache_warm}
